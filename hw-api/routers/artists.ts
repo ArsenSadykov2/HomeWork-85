199f2866713e7f3f1cb26af500d9d7a52c119384
Sadykov Arsen <LoCo10291998@gmail.com>
2025-04-25 18:53:03 +0600

    make response just for all artist

--- hw-api/routers/artists.ts
+++ hw-api/routers/artists.ts
@@ -8,15 +8,8 @@ const artistRouter = express.Router();
 
 artistRouter.get('/', async (req, res, next) => {
     try {
-        const testArtist = await Artist.create({
-            name: req.body.name,
-            image: req.body.image,
-            description: req.body.description,
-        });
-        const artists = await Artist.find({testArtist});
-        res.send({
-            artists
-        });
+        const artists = await Artist.find();
+        res.send({ artists });
     } catch (e) {
         next(e);
     }
