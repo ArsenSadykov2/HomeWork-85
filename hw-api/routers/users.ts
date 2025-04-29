66576a20a24f296633a0849460d0e532fa523aa3
Sadykov Arsen <LoCo10291998@gmail.com>
2025-04-29 14:36:28 +0600

    HW#86 - added checking for validation error while registering.

--- hw-api/routers/users.ts
+++ hw-api/routers/users.ts
@@ -1,23 +1,23 @@
 import express from "express";
 import {Error} from 'mongoose';
-import bcrypt from 'bcrypt';
 import User from "../models/User";
+import auth, {RequestWithUser} from "../middleware/auth";
 
 const usersRouter = express.Router();
 
 usersRouter.post('/', async (req, res, next) => {
-    try{
+    try {
         const user = new User({
             username: req.body.username,
             password: req.body.password,
         });
+
         user.generateToken();
         await user.save();
-        res.send(user);
-
-    }catch(error){
-        if(error instanceof Error.ValidationError){
-            res.status(400).send({error: error.message});
+        res.send({user, message: 'User registered successfully.'});
+    } catch (error) {
+        if (error instanceof Error.ValidationError) {
+            res.status(400).send(error);
             return;
         }
 
@@ -26,28 +26,38 @@ usersRouter.post('/', async (req, res, next) => {
 });
 
 usersRouter.post('/sessions', async (req, res, next) => {
-    if(!req.body.username || !req.body.password){
-        res.status(400).send({error: 'Please enter username and password'});
+    if (!req.body.username || !req.body.password) {
+        res.status(400).send({error: 'Username and password must be in req'});
+        return;
     }
 
     const user = await User.findOne({username: req.body.username});
 
-    if(!user) {
-        res.status(404).send({error: 'Username not found'});
+    if (!user) {
+        res.status(404).send({error: "Username not found"});
         return;
     }
 
     const isMath = await user.checkPassword(req.body.password);
 
-    if(!isMath) {
-        res.status(400).send({error: 'Invalid username or password'});
+    if (!isMath) {
+        res.status(400).send({error: 'Password is incorrect'});
         return;
     }
 
     user.generateToken();
     await user.save();
 
-    res.send({message: "Username and password is correct", user});
+    res.send({message: 'Username and password is correct', user});
+});
+
+usersRouter.post('/secret', auth, async (req, res, next) => {
+    const user = (req as RequestWithUser).user;
+
+    res.send({
+        message: 'Secret message',
+        user: user,
+    });
 });
 
 
